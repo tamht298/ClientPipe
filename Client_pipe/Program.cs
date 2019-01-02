ad81ba2509f9cb8bfa9e24257671b14df3e2e62d
tamht298 <REED@REED>
2019-01-02 11:09:39 +0700

    Add project files.

---
+++ Client_pipe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_pipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new NamedPipeServerStream("PipeOfPiece");
+            Console.WriteLine("------------CLIENT-------------");
+            Console.WriteLine("Client waiting connect...");
+            client.WaitForConnection();
+            Console.WriteLine("Client connected to server!");
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+            while (true)
+            {
+                Console.WriteLine("CLient:");
+
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input)) break;
+                writer.WriteLine(input);
+                writer.Flush();
+                Console.WriteLine("Server:" + reader.ReadLine());
+            }
+        }
+    }
+}
